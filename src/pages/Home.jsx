e331d91796f9cc04f3fc00d92f3277d033cf8a5d
piesieomane <piesieyeboah@gmail.com>
2022-08-08 22:23:39 +0300

    added navbar and components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <div>This is home page</div>;
+
+export default Home;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Starred = () => <div>This is starred pages</div>;
+export default Starred;
