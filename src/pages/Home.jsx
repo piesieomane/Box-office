57edc301f7758053a905ff2d7a995a6540ef9440
piesieomane <piesieyeboah@gmail.com>
2022-08-09 00:11:15 +0300

    create search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
-const Home = () => <MainPageLayout>This is home page</MainPageLayout>;
+const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onInputChange = e => {
+    setInput(e.target.value);
+  };
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(data => console.log(data));
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
 
 export default Home;
